1416ae2655b0d490b17d989badaf9c3cc88aee76
Johannes Posse <johannesposse@gmail.com>
2021-09-21 16:14:01 +0200

    delete

--- Anslagstavlan/Pages/ChatRoom/Delete.cshtml.cs
+++ Anslagstavlan/Pages/ChatRoom/Delete.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Anslagstavlan.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,20 @@ namespace Anslagstavlan.Pages.ChatRoom
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly DbCon dbcon;
+
+        public DeleteModel(DbCon dbcon)
         {
+            this.dbcon = dbcon;
+        }
+        public IActionResult OnGet(int Id)
+        {
+            var chatroom = dbcon.ChatRoomModels.Where(x => x.ChatRoomId == Id).FirstOrDefault();
+            dbcon.Remove(chatroom);
+
+            dbcon.SaveChanges();
+
+            return RedirectToPage("/ChatRoom/Index");
         }
     }
 }
