3ce1feb2b7bec02b2a1b6dd0eefb25f7feb35040
Stefan Trenh <Stefantrenh@hotmail.com>
2021-09-22 16:11:54 +0200

    Edit

--- Anslagstavlan/Pages/ChatRoom/Edit.cshtml.cs
+++ Anslagstavlan/Pages/ChatRoom/Edit.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Anslagstavlan.Data;
+using Anslagstavlan.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace Anslagstavlan.Pages.ChatRoom
 {
     public class EditModel : PageModel
     {
+        private readonly DbCon dbCon;
+
+        public ChatRoomModel ChatRoom { get; set; }
+
+        public EditModel(DbCon dbCon)
+        {
+            this.dbCon = dbCon;
+        }
         public void OnGet(int Id)
         {
+            var chatRoom = dbCon.ChatRoomModels.Where(x => x.ChatRoomId == Id).FirstOrDefault();
+            chatRoom = this.ChatRoom;
         }
 
         public void OnPost()
